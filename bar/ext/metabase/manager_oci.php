3b51b9df3627938ccd3ddc00b72c620e82da9a15
ehret <ehret>
2004-11-03 15:18:32 +0000

    metabase

---
+++ bar/ext/metabase/manager_oci.php
@@ -0,0 +1,138 @@
+<?php
+if(!defined("METABASE_MANAGER_OCI_INCLUDED"))
+{
+	define("METABASE_MANAGER_OCI_INCLUDED",1);
+
+/*
+ * manager_oci.php
+ *
+ * @(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/zvs/Repository/bar/ext/metabase/manager_oci.php,v 1.1 2004/11/03 15:18:32 ehret Exp $
+ *
+ */
+
+class metabase_manager_oci_class extends metabase_manager_database_class
+{
+	Function CreateDatabase(&$db,$name)
+	{
+		if(!IsSet($db->options[$option="DBAUser"])
+		|| !IsSet($db->options[$option="DBAPassword"]))
+			return($db->SetError("Create database","it was not specified the Oracle $option option"));
+		$success=0;
+		if($db->Connect($db->options["DBAUser"],$db->options["DBAPassword"],0))
+		{
+			if($db->DoQuery("CREATE USER ".$db->user." IDENTIFIED BY ".$db->password.(IsSet($db->options["DefaultTablespace"]) ? " DEFAULT TABLESPACE ".$db->options["DefaultTablespace"] : "")))
+			{
+				if($db->DoQuery("GRANT CREATE SESSION, CREATE TABLE,UNLIMITED TABLESPACE,CREATE SEQUENCE TO ".$db->user))
+					$success=1;
+				else
+				{
+					$error=$db->Error();
+					if(!$db->DoQuery("DROP USER ".$db->user." CASCADE"))
+						$error="could not setup the database user ($error) and then could drop its records (".$db->Error().")";
+					return($db->SetError("Create database",$error));
+				}
+			}
+		}
+		return($success);
+	}
+
+	Function DropDatabase(&$db,$name)
+	{
+		if(!IsSet($db->options[$option="DBAUser"])
+		|| !IsSet($db->options[$option="DBAPassword"]))
+			return($db->SetError("Drop database","it was not specified the Oracle $option option"));
+		return($db->Connect($db->options["DBAUser"],$db->options["DBAPassword"],0)
+		&& $db->DoQuery("DROP USER ".$db->user." CASCADE"));
+	}
+
+	Function AlterTable(&$db,$name,&$changes,$check)
+	{
+		if($check)
+		{
+			for($change=0,Reset($changes);$change<count($changes);Next($changes),$change++)
+			{
+				switch(Key($changes))
+				{
+					case "AddedFields":
+					case "RemovedFields":
+					case "ChangedFields":
+					case "name":
+						break;
+					case "RenamedFields":
+					default:
+						return($db->SetError("Alter table","change type \"".Key($changes)."\" not yet supported"));
+				}
+			}
+			return(1);
+		}
+		else
+		{
+			if(IsSet($changes["RemovedFields"]))
+			{
+				$query=" DROP (";
+				$fields=$changes["RemovedFields"];
+				for($field=0,Reset($fields);$field<count($fields);Next($fields),$field++)
+				{
+					if($field>0)
+						$query.=", ";
+					$query.=Key($fields);
+				}
+				$query.=")";
+				if(!$db->Query("ALTER TABLE $name $query"))
+					return(0);
+				$query="";
+			}
+			$query=(IsSet($changes["name"]) ? "RENAME TO ".$changes["name"] : "");
+			if(IsSet($changes["AddedFields"]))
+			{
+				$fields=$changes["AddedFields"];
+				for($field=0,Reset($fields);$field<count($fields);Next($fields),$field++)
+					$query.=" ADD (".$fields[Key($fields)]["Declaration"].")";
+			}
+			if(IsSet($changes["ChangedFields"]))
+			{
+				$fields=$changes["ChangedFields"];
+				for($field=0,Reset($fields);$field<count($fields);Next($fields),$field++)
+				{
+					$current_name=Key($fields);
+					if(IsSet($renamed_fields[$current_name]))
+					{
+						$field_name=$renamed_fields[$current_name];
+						UnSet($renamed_fields[$current_name]);
+					}
+					else
+						$field_name=$current_name;
+					$change="";
+					$change_type=$change_default=0;
+					if(IsSet($fields[$current_name]["type"]))
+						$change_type=$change_default=1;
+					if(IsSet($fields[$current_name]["length"]))
+						$change_type=1;
+					if(IsSet($fields[$current_name]["ChangedDefault"]))
+						$change_default=1;
+					if($change_type)
+						$change.=" ".$db->GetFieldTypeDeclaration($fields[$current_name]["Definition"]);
+					if($change_default)
+						$change.=" DEFAULT ".(IsSet($fields[$current_name]["Definition"]["default"]) ? $db->GetFieldValue($fields[$current_name]["Definition"]["type"],$fields[$current_name]["Definition"]["default"]) : "NULL");
+					if(IsSet($fields[$current_name]["ChangedNotNull"]))
+						$change.=(IsSet($fields[$current_name]["notnull"]) ? " NOT" : "")." NULL";
+					if(strcmp($change,""))
+						$query.=" MODIFY ($field_name$change)";
+				}
+			}
+			return($query=="" || $db->Query("ALTER TABLE $name $query"));
+		}
+	}
+
+	Function CreateSequence(&$db,$name,$start)
+	{
+		return($db->Query("CREATE SEQUENCE $name START WITH $start INCREMENT BY 1".($start<1 ? " MINVALUE $start" : "")));
+	}
+
+	Function DropSequence(&$db,$name)
+	{
+		return($db->Query("DROP SEQUENCE $name"));
+	}
+};
+}
+?>
---
+++ bar/ext/metabase/manager_pgsql.php
@@ -0,0 +1,111 @@
+<?php
+if(!defined("METABASE_MANAGER_PGSQL_INCLUDED"))
+{
+	define("METABASE_MANAGER_PGSQL_INCLUDED",1);
+
+/*
+ * manager_pgsql.php
+ *
+ * @(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/zvs/Repository/bar/ext/metabase/manager_pgsql.php,v 1.1 2004/11/03 15:18:32 ehret Exp $
+ *
+ */
+
+class metabase_manager_pgsql_class extends metabase_manager_database_class
+{
+	Function StandaloneQuery(&$db,$query)
+	{
+		if(($connection=$db->DoConnect("template1",0))==0)
+			return(0);
+		if(!($success=@pg_Exec($connection,"$query")))
+			$db->SetError("Standalone query",pg_ErrorMessage($connection));
+		pg_Close($connection);
+		return($success);
+	}
+
+	Function CreateDatabase(&$db,$name)
+	{
+		return($this->StandaloneQuery($db,"CREATE DATABASE $name"));
+	}
+
+	Function DropDatabase(&$db,$name)
+	{
+		return($this->StandaloneQuery($db,"DROP DATABASE $name"));
+	}
+
+	Function AlterTable(&$db,$name,&$changes,$check)
+	{
+		if($check)
+		{
+			for($change=0,Reset($changes);$change<count($changes);Next($changes),$change++)
+			{
+				switch(Key($changes))
+				{
+					case "AddedFields":
+						break;
+					case "RemovedFields":
+						return($db->SetError("Alter table","database server does not support dropping table columns"));
+					case "name":
+					case "RenamedFields":
+					case "ChangedFields":
+					default:
+						return($db->SetError("Alter table","change type \"".Key($changes)."\" not yet supported"));
+				}
+			}
+			return(1);
+		}
+		else
+		{
+			if(IsSet($changes[$change="name"])
+			|| IsSet($changes[$change="RenamedFields"])
+			|| IsSet($changes[$change="ChangedFields"]))
+				return($db->SetError("Alter table","change type \"$change\" not yet supported"));
+			$query="";
+			if(IsSet($changes["AddedFields"]))
+			{
+				$fields=$changes["AddedFields"];
+				for($field=0,Reset($fields);$field<count($fields);Next($fields),$field++)
+				{
+					if(!$db->Query("ALTER TABLE $name ADD ".$fields[Key($fields)]["Declaration"]))
+						return(0);
+				}
+			}
+			if(IsSet($changes["RemovedFields"]))
+			{
+				$fields=$changes["RemovedFields"];
+				for($field=0,Reset($fields);$field<count($fields);Next($fields),$field++)
+				{
+					if(!$db->Query("ALTER TABLE $name DROP ".Key($fields)))
+						return(0);
+				}
+			}
+			return(1);
+		}
+	}
+
+	Function CreateSequence(&$db,$name,$start)
+	{
+		return($db->Query("CREATE SEQUENCE $name INCREMENT 1".($start<1 ? " MINVALUE $start" : "")." START $start"));
+	}
+
+	Function DropSequence(&$db,$name)
+	{
+		return($db->Query("DROP SEQUENCE $name"));
+	}
+
+	Function GetSequenceCurrentValue(&$db,$name,&$value)
+	{
+		if(!($result=$db->Query("SELECT last_value FROM $name")))
+			return(0);
+		if($db->NumberOfRows($result)==0)
+		{
+			$db->FreeResult($result);
+			return($db->SetError("Get sequence current value","could not find value in sequence table"));
+		}
+		$value=intval($db->FetchResult($result,0,0));
+		$db->FreeResult($result);
+		return(1);
+	}
+
+};
+}
+?>
