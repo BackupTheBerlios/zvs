cf1951b862fd7ce2e16fa7d7071c009ba6b4c707
ehret <ehret>
2004-12-10 18:11:44 +0000

    add new functions for statistics per hour modified existing get function to also list per hour

--- bar/includes/statisticsclass.inc.php
+++ bar/includes/statisticsclass.inc.php
@@ -1,29 +1,29 @@
 <?php
-/***************************************************************
-*  Copyright notice
-*  
-*  (c) 2003-2004 Christian Ehret (chris@ehret.name)
-*  All rights reserved
-*
-*  This script is part of the ZVS project. The ZVS project is 
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
+/**
+* Copyright notice
 * 
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*  A copy is found in the textfile GPL.txt and important notices to the license 
-*  from the author is found in LICENSE.txt distributed with these scripts.
-*
+*    (c) 2003-2004 Christian Ehret (chris@ehret.name)
+*    All rights reserved
 * 
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+*    This script is part of the ZVS project. The ZVS project is 
+*    free software; you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation; either version 2 of the License, or
+*    (at your option) any later version.
+* 
+*    The GNU General Public License can be found at
+*    http://www.gnu.org/copyleft/gpl.html.
+*    A copy is found in the textfile GPL.txt and important notices to the license 
+*    from the author is found in LICENSE.txt distributed with these scripts.
+* 
+* 
+*    This script is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+* 
+*    This copyright notice MUST APPEAR in all copies of the script!
+*/
 
 /**
 * class Statistics
@@ -34,7 +34,7 @@
 * 
 * @since 2004-01-19
 * @author Christian Ehret <chris@uffbasse.de> 
-* @version $Id: statisticsclass.inc.php,v 1.2 2004/11/03 16:33:52 ehret Exp $
+* @version $Id: statisticsclass.inc.php,v 1.3 2004/12/10 18:11:44 ehret Exp $
 */
 class Statistics {
     /**
@@ -44,28 +44,52 @@ class Statistics {
     * 
     * @param string $thestart start date
     * @param string $theend end date
-	* @param int $id category
+    * @param int $id category
+    * @param int $from from hour
+    * @param int $till till hour
     * @return array statistic
     * @access public 
     * @since 2003-07-24
     * @author Christian Ehret <chris@uffbasse.de> 
     */
-    function get($thestart, $theend, $id)
+    function get($thestart, $theend, $id, $from = 0, $till = 23)
     {
         global $gDatabase, $tbl_bought, $tbl_bararticle, $request, $errorhandler;
 
         $statistic = array();
-        $query = "SELECT sum(num), description, price
+        $query = "SELECT sum(num), description, price, DATE_FORMAT( timestamp,  '%H'  )
 		                 FROM $tbl_bought 
 						 LEFT JOIN $tbl_bararticle ON $tbl_bought.fk_bararticle_id = $tbl_bararticle.pk_bararticle_id
 						 WHERE timestamp >= '$thestart'
 						 AND timestamp <= '$theend' ";
-		if ($id != -1) {
-		    $query .= "  AND $tbl_bararticle.fk_bararticlecat_id = $id "; 
-		}						 
-		$query .="		 GROUP BY description, price
+        if ($id != -1) {
+            $query .= "  AND $tbl_bararticle.fk_bararticlecat_id = $id ";
+        } 
+        if (!($from == 0 && $till == 23)) {
+            $query .= " AND (";
+            if ($from <= $till) {
+                for ($i = $from; $i <= $till; $i++) {
+                    $query .= "DATE_FORMAT( timestamp,  '%H'  ) = $i";
+                    if ($i != $till) {
+                        $query .= " OR ";
+                    } 
+                } 
+            } else {
+                for ($i = $from; $i <= 23; $i++) {
+                    $query .= "DATE_FORMAT( timestamp,  '%H'  ) = $i";
+                    $query .= " OR ";
+                } 
+                for ($i = 0; $i <= $till; $i++) {
+                    $query .= "DATE_FORMAT( timestamp,  '%H'  ) = $i";
+                    if ($i != $till) {
+                        $query .= " OR ";
+                    } 
+                } 
+            } 
+            $query .= ") ";
+        } 
+        $query .= "		 GROUP BY description, price
 						 ORDER BY description ";
-
         $result = MetabaseQuery($gDatabase, $query);
         if (!$result) {
             $errorhandler->display('SQL', 'Statistics::get()', $query);
@@ -101,6 +125,145 @@ class Statistics {
         return $statistic;
     } 
 
+    /**
+    * Statistics::getByHour()
+    * 
+    * This function returns statistics of articles by hour.
+    * 
+    * @param string $thestart start date
+    * @param string $theend end date
+    * @param int $id category
+    * @param int $from from hour
+    * @param int $till till hour
+    * @return array statistic
+    * @access public 
+    * @since 2004-12-10
+    * @author Christian Ehret <chris@uffbasse.de> 
+    */
+    function getByHour($thestart, $theend, $id, $from = 0, $till = 23)
+    {
+        global $gDatabase, $tbl_bought, $tbl_bararticle, $request, $errorhandler;
+
+        $statistic = array();
+        $query = "SELECT sum(num), sum( num * price ), DATE_FORMAT( timestamp, '%H' ) AS HOUR
+		                 FROM $tbl_bought 
+						 LEFT JOIN $tbl_bararticle ON $tbl_bought.fk_bararticle_id = $tbl_bararticle.pk_bararticle_id
+						 WHERE timestamp >= '$thestart'
+						 AND timestamp <= '$theend' ";
+        if ($id != -1) {
+            $query .= "  AND $tbl_bararticle.fk_bararticlecat_id = $id ";
+        } 
+        if (!($from == 0 && $till == 23)) {
+            $query .= " AND (";
+            if ($from <= $till) {
+                for ($i = $from; $i <= $till; $i++) {
+                    $query .= "DATE_FORMAT( timestamp,  '%H'  ) = $i";
+                    if ($i != $till) {
+                        $query .= " OR ";
+                    } 
+                } 
+            } else {
+                for ($i = $from; $i <= 23; $i++) {
+                    $query .= "DATE_FORMAT( timestamp,  '%H'  ) = $i";
+                    $query .= " OR ";
+                } 
+                for ($i = 0; $i <= $till; $i++) {
+                    $query .= "DATE_FORMAT( timestamp,  '%H'  ) = $i";
+                    if ($i != $till) {
+                        $query .= " OR ";
+                    } 
+                } 
+            } 
+            $query .= ") ";
+        } 
+        $query .= "		 GROUP BY HOUR
+						 ORDER BY HOUR ";
+        $result = MetabaseQuery($gDatabase, $query);
+        if (!$result) {
+            $errorhandler->display('SQL', 'Statistics::get()', $query);
+        } else {
+            $row = 0;
+            for ($row = 0; ($eor = MetabaseEndOfResult($gDatabase, $result)) == 0; ++$row) {
+                $statistic[$row] = array ('num' => MetabaseFetchResult($gDatabase, $result, $row, 0),
+                    'price' => MetabaseFetchResult($gDatabase, $result, $row, 1),
+                    'hour' => number_format(MetabaseFetchResult($gDatabase, $result, $row, 2)),
+                    'color' => $color,
+                    );
+            } 
+        } 
+        return $statistic;
+    } 	
+
+
+    /**
+    * Statistics::getByDayAndHour()
+    * 
+    * This function returns statistics of articles by day and hour.
+    * 
+    * @param string $thestart start date
+    * @param string $theend end date
+    * @param int $id category
+    * @param int $from from hour
+    * @param int $till till hour
+    * @return array statistic
+    * @access public 
+    * @since 2004-12-10
+    * @author Christian Ehret <chris@uffbasse.de> 
+    */
+    function getByDayAndHour($thestart, $theend, $id, $from = 0, $till = 23)
+    {
+        global $gDatabase, $tbl_bought, $tbl_bararticle, $request, $errorhandler;
+
+        $statistic = array();
+        $query = "SELECT sum(num), sum( num * price ), DATE_FORMAT( timestamp, '%d.%m.%Y' ) AS DAY, DATE_FORMAT( timestamp, '%H' ) AS HOUR
+		                 FROM $tbl_bought 
+						 LEFT JOIN $tbl_bararticle ON $tbl_bought.fk_bararticle_id = $tbl_bararticle.pk_bararticle_id
+						 WHERE timestamp >= '$thestart'
+						 AND timestamp <= '$theend' ";
+        if ($id != -1) {
+            $query .= "  AND $tbl_bararticle.fk_bararticlecat_id = $id ";
+        } 
+        if (!($from == 0 && $till == 23)) {
+            $query .= " AND (";
+            if ($from <= $till) {
+                for ($i = $from; $i <= $till; $i++) {
+                    $query .= "DATE_FORMAT( timestamp,  '%H'  ) = $i";
+                    if ($i != $till) {
+                        $query .= " OR ";
+                    } 
+                } 
+            } else {
+                for ($i = $from; $i <= 23; $i++) {
+                    $query .= "DATE_FORMAT( timestamp,  '%H'  ) = $i";
+                    $query .= " OR ";
+                } 
+                for ($i = 0; $i <= $till; $i++) {
+                    $query .= "DATE_FORMAT( timestamp,  '%H'  ) = $i";
+                    if ($i != $till) {
+                        $query .= " OR ";
+                    } 
+                } 
+            } 
+            $query .= ") ";
+        } 
+        $query .= "		 GROUP BY DAY, HOUR
+						 ORDER BY HOUR ";
+        $result = MetabaseQuery($gDatabase, $query);
+        if (!$result) {
+            $errorhandler->display('SQL', 'Statistics::get()', $query);
+        } else {
+            $row = 0;
+            for ($row = 0; ($eor = MetabaseEndOfResult($gDatabase, $result)) == 0; ++$row) {
+                $statistic[$row] = array ('num' => MetabaseFetchResult($gDatabase, $result, $row, 0),
+                    'price' => MetabaseFetchResult($gDatabase, $result, $row, 1),
+                    'hour' => MetabaseFetchResult($gDatabase, $result, $row, 2),
+                    'color' => $color,
+                    );
+            } 
+        } 
+        return $statistic;
+    } 	
+	
     /**
     * Statistics::getNow()
     * 
