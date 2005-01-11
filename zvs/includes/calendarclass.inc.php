c098c5501cabcabdd1f92454436cc09d8a0397e1
ehret <ehret>
2005-01-11 09:18:18 +0000

    getdates added

--- zvs/includes/calendarclass.inc.php
+++ zvs/includes/calendarclass.inc.php
@@ -1,29 +1,29 @@
 <?php
-/***************************************************************
-*  Copyright notice
-*  
-*  (c) 2003-2004 Christian Ehret (chris@ehret.name)
-*  All rights reserved
-*
-*  This script is part of the ZVS project. The ZVS project is 
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
+/**
+* Copyright notice
 * 
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*  A copy is found in the textfile GPL.txt and important notices to the license 
-*  from the author is found in LICENSE.txt distributed with these scripts.
-*
+*   (c) 2003-2004 Christian Ehret (chris@ehret.name)
+*   All rights reserved
 * 
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+*   This script is part of the ZVS project. The ZVS project is 
+*   free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation; either version 2 of the License, or
+*   (at your option) any later version.
+* 
+*   The GNU General Public License can be found at
+*   http://www.gnu.org/copyleft/gpl.html.
+*   A copy is found in the textfile GPL.txt and important notices to the license 
+*   from the author is found in LICENSE.txt distributed with these scripts.
+* 
+* 
+*   This script is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+* 
+*   This copyright notice MUST APPEAR in all copies of the script!
+*/
 
 /**
 * class Calendar
@@ -34,7 +34,7 @@
 * 
 * @since 2003-07-24
 * @author Christian Ehret <chris@uffbasse.de> 
-* @version $Id: calendarclass.inc.php,v 1.1 2004/11/03 14:43:03 ehret Exp $
+* @version $Id: calendarclass.inc.php,v 1.2 2005/01/11 09:18:18 ehret Exp $
 */
 class Calendar {
     /**
@@ -296,8 +296,8 @@ class Calendar {
                         } 
 
                         if ($startstr == $linkDate) {
-                            $firstday = "true";
-                            //$lastdaydata = array();
+                            $firstday = "true"; 
+                            // $lastdaydata = array();
                         } 
 
                         if ($endstr == $linkDate) {
@@ -515,6 +515,45 @@ class Calendar {
             } 
             return $days;
         } 
+    } 
+
+    /**
+    * Calendar::getdates()
+    * 
+    * This function returns an array with all dates.
+    * 
+    * @return array dates
+    * @access public 
+    * @since 2005-01-11
+    * @author Christian Ehret <chris@uffbasse.de> 
+    */
+    function getdates()
+    {
+        global $gDatabase, $tbl_booking_detail, $request, $errorhandler;
+
+        $dates = array();
+        $j = 0;
+        $query = "SELECT DATE_FORMAT(min( start_date  ) ,'%Y')  
+		                 FROM $tbl_booking_detail  ";
+        $result = MetabaseQuery($gDatabase, $query);
+        if (!$result) {
+            $errorhandler->display('SQL', 'Calendar::getdates()', $query);
+        } else {
+            $startyear = MetabaseFetchResult($gDatabase, $result, 0, 0);
+            $todaydate = getdate();
+            $todayyear = $todaydate['year'];
+            $i = 0;
+            for ($year = $startyear; $year <= $todayyear; ++$year) {
+                $dates[$i] = $year;
+                ++$i;
+            } 
+            for ($j = 1; $j <= 20; ++$j) {
+                $dates[$i] = $year + $j;
+                ++$i;
+            } 
+
+        } 
+        return $dates;
     } 
 } 
 
